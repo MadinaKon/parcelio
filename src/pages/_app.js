f1430d02a84ec2a443582460b5a69e14b8c698f5
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-14 10:36:07 +0200

    install swr and add fetcher

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { SWRConfig } from "swr";
+
+const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <SWRConfig value={{ fetcher }}>
+        <Component {...pageProps} />
+      </SWRConfig>
+    </>
+  );
 }
