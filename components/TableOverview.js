b8a0089f1caf8a06a808cc8ae3dd9e934ddcdbac
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-15 14:04:48 +0200

    display services collection data from DB on the table overview

--- components/TableOverview.js
+++ components/TableOverview.js
@@ -5,82 +5,43 @@ import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-// import Title from "./Title";
-
-// Generate Order Data
-function createData(id, date, name, shipTo, paymentMethod, amount) {
-  return { id, date, name, shipTo, paymentMethod, amount };
-}
-
-const rows = [
-  createData(
-    0,
-    "16 Mar, 2019",
-    "Elvis Presley",
-    "Tupelo, MS",
-    "VISA ⠀•••• 3719",
-    312.44
-  ),
-  createData(
-    1,
-    "16 Mar, 2019",
-    "Paul McCartney",
-    "London, UK",
-    "VISA ⠀•••• 2574",
-    866.99
-  ),
-  createData(
-    2,
-    "16 Mar, 2019",
-    "Tom Scholz",
-    "Boston, MA",
-    "MC ⠀•••• 1253",
-    100.81
-  ),
-  createData(
-    3,
-    "16 Mar, 2019",
-    "Michael Jackson",
-    "Gary, IN",
-    "AMEX ⠀•••• 2000",
-    654.39
-  ),
-  createData(
-    4,
-    "15 Mar, 2019",
-    "Bruce Springsteen",
-    "Long Branch, NJ",
-    "VISA ⠀•••• 5919",
-    212.79
-  ),
-];
 
 function preventDefault(event) {
   event.preventDefault();
 }
 
-export default function TableOverview() {
+export default function TableOverview({ data }) {
   return (
     <React.Fragment>
       <title>Table overview page</title>
       <Table size="small">
         <TableHead>
           <TableRow>
-            <TableCell>Date</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Ship To</TableCell>
-            <TableCell>Payment Method</TableCell>
-            <TableCell align="right">Sale Amount</TableCell>
+            <TableCell>Firstname</TableCell>
+            <TableCell>Lastname</TableCell>
+            <TableCell>Username</TableCell>
+            <TableCell>From city</TableCell>
+            <TableCell>To city</TableCell>
+            <TableCell>Flight Date time</TableCell>
+            <TableCell>Available kilos</TableCell>
+            <TableCell>Phone number</TableCell>
+
+            {/* <TableCell align="right">Sale Amount</TableCell> */}
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {data.map((row) => (
             <TableRow key={row.id}>
-              <TableCell>{row.date}</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.shipTo}</TableCell>
-              <TableCell>{row.paymentMethod}</TableCell>
-              <TableCell align="right">{`$${row.amount}`}</TableCell>
+              <TableCell>{row.firstName}</TableCell>
+              <TableCell>{row.lastName}</TableCell>
+              <TableCell>{row.userName}</TableCell>
+              <TableCell>{row.fromCity}</TableCell>
+              <TableCell>{row.toCity}</TableCell>
+              <TableCell>{row.flightDateTime}</TableCell>
+              <TableCell>{row.availableKilos}</TableCell>
+              <TableCell>{row.phoneNumber}</TableCell>
+              {/* <TableCell>{row.paymentMethod}</TableCell> */}
+              {/* <TableCell align="right">{`$${row.amount}`}</TableCell> */}
             </TableRow>
           ))}
         </TableBody>
