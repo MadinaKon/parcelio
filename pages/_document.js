186e8c97f3e35a0e4a30905198d40e8f9bca2dd3
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-15 13:50:11 +0200

    add material ui table overview example

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ export default function Document() {
         rel="stylesheet"
         href="https://fonts.googleapis.com/icon?family=Material+Icons"
       />
+
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
       <link
