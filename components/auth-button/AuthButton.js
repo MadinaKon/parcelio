44cc446d6b26087f5666599fb58b1db75fda5016
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-16 15:36:51 +0200

    add components for auth

---
+++ components/auth-button/AuthButton.js
@@ -0,0 +1,28 @@
+import { btn, logout, login } from "./AuthButton.module.css";
+import { useSession, signIn, signOut } from "next-auth/react";
+
+export default function AuthButton() {
+  const { data: session } = useSession();
+
+  console.log("SESSION ", session);
+
+  if (session) {
+    return (
+      <>
+        <button className={`${btn} ${logout}`} onClick={() => signOut()}>
+          Sign out
+        </button>
+      </>
+    );
+  }
+  return (
+    <>
+      <button className={`${btn} ${login}`} onClick={() => signIn("github")}>
+        Sign in with GitHub
+      </button>
+      <button className={`${btn} ${login}`} onClick={() => signIn("google")}>
+        Sign in with Google
+      </button>
+    </>
+  );
+}
