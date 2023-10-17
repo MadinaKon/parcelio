2308cfe24c31da6de4fbf8d8bcbc3543b4e42aa7
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-17 15:07:51 +0200

    add Sender model and schema

---
+++ db/models/Sender.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import "./Service";
+
+const { Schema } = mongoose;
+
+const senderSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  userName: { type: String, required: true },
+  address: { type: String, required: true },
+  postalCode: { type: String, required: true },
+  country: { type: String, required: true },
+  city: { type: String, required: true },
+  description: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  services: { type: [Schema.Types.ObjectId], ref: "Service" },
+});
+
+const Sender = mongoose.models.Sender || mongoose.model("Sender", senderSchema);
+
+export default Sender;
