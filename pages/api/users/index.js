3e18770aea8cc60f381adec3a2a788b9e1d72301
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-18 14:30:18 +0200

    rename Sender to Package

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,12 +1,9 @@
-// import Sender from "../../../db/models/Package";
-import Sender from "../../../db/models/Package";
 import dbConnect from "../../../db/models/connect";
 import User from "../../../db/models/User";
 
 export default async function handler(request, response) {
   await dbConnect();
 
-  // Write the POST API route in pages/api/places/index.js
   if (request.method === "GET") {
     const users = await User.find();
     return response.status(200).json(users);
@@ -17,7 +14,7 @@ export default async function handler(request, response) {
       const data = request.body;
       await User.create(data);
 
-      response.status(201).json({ status: "user is created" });
+      response.status(201).json({ status: "a new user is created" });
     } catch (error) {
       console.log(error);
       response.status(400).json({ error: error.message });
