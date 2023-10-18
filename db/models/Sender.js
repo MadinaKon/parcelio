b5a07839d59b362d39d72451367b615b4d6f29cc
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-18 14:04:34 +0200

    add properties to packageSchema

--- db/models/Sender.js
+++ db/models/Sender.js
@@ -1,25 +1,25 @@
 import mongoose from "mongoose";
 import "./Service";
+import "./User";
 
 const { Schema } = mongoose;
 
 const directionEnum = ["intracity", "Central asia", "international"];
 
-const senderSchema = new Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  // userName: { type: String, required: true },
-  country: { type: String, required: true },
-  city: { type: String, required: true },
-  address: { type: String, required: true },
-  postalCode: { type: String, required: true },
-  phoneNumber: { type: String, required: true },
+const packageSchema = new Schema({
   enum: directionEnum,
-  // description: { type: String, required: true },
-  // weight: { type: String, required: true },
+  length: { type: String, required: true },
+  width: { type: String, required: true },
+  height: { type: String, required: true },
+  totalWeight: { type: String, required: true },
+  packageType: { type: String, required: true },
+  description: { type: String, required: true },
+  // recipientAddress: { type: String, required: true },
+  users: { type: [Schema.Types.ObjectId], ref: "User" },
   services: { type: [Schema.Types.ObjectId], ref: "Service" },
 });
 
-const Sender = mongoose.models.Sender || mongoose.model("Sender", senderSchema);
+const Package =
+  mongoose.models.Package || mongoose.model("Package", packageSchema);
 
-export default Sender;
+export default Package;
