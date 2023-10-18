1038eefe438f7efa42564527141ca4991b57ebc5
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-18 13:47:22 +0200

    sign up form sends a request to api/senders and stores a new sender in DB

--- db/models/Sender.js
+++ db/models/Sender.js
@@ -8,16 +8,15 @@ const directionEnum = ["intracity", "Central asia", "international"];
 const senderSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  userName: { type: String, required: true },
+  // userName: { type: String, required: true },
   country: { type: String, required: true },
   city: { type: String, required: true },
   address: { type: String, required: true },
   postalCode: { type: String, required: true },
-  description: { type: String, required: true },
   phoneNumber: { type: String, required: true },
   enum: directionEnum,
-  //default: "international",
-  weight: { type: String, required: true },
+  // description: { type: String, required: true },
+  // weight: { type: String, required: true },
   services: { type: [Schema.Types.ObjectId], ref: "Service" },
 });
 
