e97bb89c9b131ad58668bbc6ac578dba52304c0c
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-19 20:57:08 +0200

    button text is either Contact or Update

--- components/modal/BasicModal.js
+++ components/modal/BasicModal.js
@@ -39,7 +39,6 @@ export default function BasicModal({ children }) {
     <div>
       {session ? (
         <>
-          <Button onClick={handleOpen}>Contact</Button>
           <Button onClick={handleOpen}>Update</Button>
         </>
       ) : (
