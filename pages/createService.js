f7f349dcd62b49c0f8d19a940b4ea735d78d41a5
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-21 20:00:10 +0200

    remove comments

--- pages/createService.js
+++ pages/createService.js
@@ -14,22 +14,6 @@ export default function CreateServicePage() {
   const { mutate } = useSWR("/api/services");
   const router = useRouter();
 
-  // async function addService(service) {
-  //   const response = await fetch("/api/services", {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify(service),
-  //   });
-
-  //   if (response.ok) {
-  //     mutate();
-  //   }
-
-  //   router.push("/");
-  // }
-
   return (
     <>
       <h2 id="add-service">Add Service</h2>
