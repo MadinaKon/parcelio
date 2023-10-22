7064d750b6fd56885b55109e114ad962ef0e90ed
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-22 19:01:26 +0200

    start with email provider in next-auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -26,6 +26,10 @@ export const authOptions = {
         },
       },
     }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
   ],
   adapter: MongoDBAdapter(clientPromise),
   callbacks: {
