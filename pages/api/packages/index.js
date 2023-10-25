56c713d4fd43a271f0eb92288a7d52ba6329ddb0
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-25 18:10:04 +0200

    remove unused from pages/api/packages/index.js

--- pages/api/packages/index.js
+++ pages/api/packages/index.js
@@ -11,49 +11,11 @@ export default async function handler(request, response) {
     return response.status(200).json(packages);
   }
 
-  // if (request.method === "POST") {
-  //   // console.log("WHICH ID ", id);
-  //   try {
-  //     const { request, serviceId, userId } = request.body;
-  //     await Package.create({ request, serviceId });
-  //     console.log("REQUEST ", request);
-  //     console.log("SERVICE ID ", serviceId);
-  //     console.log("USER ID ", userId);
-
-  //     await User.findByIdAndUpdate(
-  //       id,
-  //       {
-  //         $push: { notifications: data.serviceId },
-  //       },
-  //       { new: true }
-  //     );
-
-  //     response.status(201).json({ status: "package successfully created" });
-  //   } catch (error) {
-  //     console.log(error);
-  //     response.status(400).json({ error: error.message });
-  //   }
-  // }
-
   if (request.method === "POST") {
-    // console.log("WHICH ID ", id);
     try {
       const { serviceId, userId, ...requestBody } = request.body;
-      // await Package.create({ requestBody, serviceId });
 
       await Package.create({ serviceId, userId, ...requestBody });
-
-      console.log("REQUEST ", requestBody);
-      console.log("SERVICE ID ", serviceId);
-      console.log("USER ID ", userId);
-
-      // await User.findByIdAndUpdate(
-      //   id,
-      //   {
-      //     $push: { notifications: data.serviceId },
-      //   },
-      //   { new: true }
-      // );
 
       await User.findByIdAndUpdate(
         userId,
