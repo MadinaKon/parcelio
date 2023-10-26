dc98521a6b6e6884ab938dd68fbc9671c0944445
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-26 11:28:02 +0200

    do not use table for notifications display on Profile page

--- components/notification-table/NotificationTable.js
+++ components/notification-table/NotificationTable.js
@@ -8,30 +8,6 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
 export default function NotificationTable({ defaultData }) {
-  // console.log("DEFAULT DATA IN NOTIFICATION TABLE ", defaultData);
-  return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Date/Time</TableCell>
-            <TableCell align="right">Notification</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {defaultData?.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+  console.log("DEFAULT DATA IN NOTIFICATION TABLE ", defaultData);
+  return <></>;
 }
