a4282819bd24f029b6270484ea5d2e6063676557
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-26 11:28:33 +0200

    remove unused imports

--- components/notification-table/NotificationTable.js
+++ components/notification-table/NotificationTable.js
@@ -1,11 +1,4 @@
 import React, { Fragment } from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
 
 export default function NotificationTable({ defaultData }) {
   console.log("DEFAULT DATA IN NOTIFICATION TABLE ", defaultData);
