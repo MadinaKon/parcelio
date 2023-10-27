917bb19d30e7d627b7b81d1a28de521a68ea1437
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-27 16:40:06 +0200

    add session for /createService

--- pages/createService.js
+++ pages/createService.js
@@ -5,22 +5,35 @@ import { StyledLink } from "../components/StyledLink.js";
 import useSWR from "swr";
 import TransporterForm from "../components/form/TransporterForm.js";
 import SenderForm from "../components/form/SenderForm.js";
-
+import { useSession } from "next-auth/react";
+import { useState } from "react";
 const StyledBackLink = styled(StyledLink)`
   justify-self: flex-start;
 `;
 
 export default function CreateServicePage() {
   const { mutate } = useSWR("/api/services");
+
   const router = useRouter();
+  const { data: session } = useSession();
+
+  // if (!session) {
+  //   // router.push("/");
+  //   router.push()
+  // }
 
   return (
     <>
-      <h2 id="add-service">Add Service</h2>
-      <Link href="/" passHref legacyBehavior>
-        <StyledBackLink>back</StyledBackLink>
-      </Link>
-      <TransporterForm formName={"add-service"} />
+      {session && (
+        <>
+          {" "}
+          <h2 id="add-service">Add Service</h2>
+          <Link href="/" passHref legacyBehavior>
+            <StyledBackLink>back</StyledBackLink>
+          </Link>
+          <TransporterForm formName={"add-service"} />
+        </>
+      )}
     </>
   );
 }
