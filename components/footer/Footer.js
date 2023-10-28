bf6bf69d10fd440a831824e3938e7c5a17e74cf2
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-28 15:05:48 +0200

    add footer with styling and position fixed, bottom:0

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,5 +1,5 @@
 import { footer } from "./Footer.module.css";
 
 export default function Footer() {
-  return <footer className={footer}>Made at Spiced 🌶️ Academy</footer>;
+  return <footer className={footer}>Made by Spiced 🌶️ Academy graduate</footer>;
 }
