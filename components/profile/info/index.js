37a4d452ead4097ea88d7094a891f1c829a16a86
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-28 16:32:19 +0200

    add Avatar from @mui/material and remove margin-top from image ti display MUI avatar

--- components/profile/info/index.js
+++ components/profile/info/index.js
@@ -1,19 +1,22 @@
 import Image from "next/image";
 import AuthButton from "../../auth-button/AuthButton";
+// import ImageAvatars from "./ImageAvatars";
+import { Avatar } from "@mui/material";
 
 export default function ProfileInfo({ session }) {
   return (
     <>
-      <Image
+      {/* <Image
         width={150}
         height={150}
         src={session.user?.image || `/assets/images/default.png`}
         alt="Avatar"
         priority
-      />
+      /> */}
       <div>
         <h1>Hello {session.user?.name}</h1>
         <p>You are signed in as {session.user?.email}</p>
+        <Avatar alt="Remy Sharp" src={session.user?.image} />
         <AuthButton />
       </div>
     </>
