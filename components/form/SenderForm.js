8918a4c7b2a112f160d2dd5bd1518b90bc447270
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-28 18:15:58 +0200

    add Grid container for form

--- components/form/SenderForm.js
+++ components/form/SenderForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useRouter } from "next/router";
 import useSWR from "swr";
+import { Grid } from "@mui/material";
 
 export default function SenderForm({
   formName,
@@ -44,98 +45,126 @@ export default function SenderForm({
 
   return (
     <form aria-labelledby={formName} onSubmit={handleSubmit}>
-      <h3>Send the details of the package</h3>
+      <Grid container>
+        <Grid item xs={12}>
+          <h4>Send the details of the package</h4>
+        </Grid>
+        {/* {defaultData ? "Update service details" : "Send the details of the package"} */}
+        <Grid item xs={6}>
+          <label htmlFor="email">Email</label>
+        </Grid>
+        <Grid item xs={6}>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            defaultValue={defaultData?.email}
+            required
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <label htmlFor="phone">Phone number</label>
+        </Grid>
+        <Grid item xs={6}>
+          <input
+            id="phone"
+            name="phoneNumber"
+            type="text"
+            defaultValue={defaultData?.phone}
+            required
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <label htmlFor="packageType">Package type</label>
+        </Grid>
+        <Grid item xs={6}>
+          <input
+            id="packageType"
+            name="packageType"
+            type="text"
+            defaultValue={defaultData?.packageType}
+            required
+          />
+        </Grid>
+        <Grid item xs={6}>
+          {" "}
+          <label htmlFor="totalWeight">Total weight</label>
+        </Grid>
+        <Grid item xs={6}>
+          <input
+            id="totalWeight"
+            name="totalWeight"
+            type="text"
+            defaultValue={defaultData?.totalWeight}
+            required
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <label htmlFor="height">Height</label>
+        </Grid>
+        <Grid item xs={6}>
+          <input
+            id="height"
+            name="height"
+            type="text"
+            defaultValue={defaultData?.height}
+            required
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <label htmlFor="width">Width</label>
+        </Grid>
+        <Grid item xs={6}>
+          <input
+            id="width"
+            name="width"
+            type="text"
+            defaultValue={defaultData?.height}
+            required
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <label htmlFor="length">Length</label>
+        </Grid>
+        <Grid item xs={6}>
+          <input
+            id="length"
+            name="length"
+            type="text"
+            defaultValue={defaultData?.height}
+            required
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <label htmlFor="weight">Weight</label>
+        </Grid>
+        <Grid item xs={6}>
+          <input
+            name="weight"
+            id="weight"
+            type="text"
+            defaultValue={defaultData?.weight}
+            required
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <label htmlFor="description">Description</label>
+        </Grid>
+        <Grid item xs={6}>
+          <textarea
+            name="description"
+            id="description"
+            cols="16"
+            rows="10"
+            defaultValue={defaultData?.description}
+          ></textarea>
+        </Grid>
 
-      {/* {defaultData ? "Update service details" : "Send the details of the package"} */}
-      <br />
-      <label htmlFor="email">Email</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        defaultValue={defaultData?.email}
-        required
-      />
-      <br />
-      <label htmlFor="phone">Phone number</label>
-      <input
-        id="phone"
-        name="phoneNumber"
-        type="text"
-        defaultValue={defaultData?.phone}
-        required
-      />
-      <br />
-      <label htmlFor="packageType">Package type</label>
-      <input
-        id="packageType"
-        name="packageType"
-        type="text"
-        defaultValue={defaultData?.packageType}
-        required
-      />
-      <br />
-      <label htmlFor="totalWeight">Total weight</label>
-      <input
-        id="totalWeight"
-        name="totalWeight"
-        type="text"
-        defaultValue={defaultData?.totalWeight}
-        required
-      />
-      <br />
-
-      <label htmlFor="height">Height</label>
-      <input
-        id="height"
-        name="height"
-        type="text"
-        defaultValue={defaultData?.height}
-        required
-      />
-      <br />
-
-      <label htmlFor="width">Width</label>
-      <input
-        id="width"
-        name="width"
-        type="text"
-        defaultValue={defaultData?.height}
-        required
-      />
-      <br />
-      <label htmlFor="length">Length</label>
-      <input
-        id="length"
-        name="length"
-        type="text"
-        defaultValue={defaultData?.height}
-        required
-      />
-      <br />
-      <label htmlFor="weight">Weight</label>
-      <input
-        name="weight"
-        id="weight"
-        type="text"
-        defaultValue={defaultData?.weight}
-        required
-      />
-      <br />
-      <label htmlFor="description">Description</label>
-      <textarea
-        name="description"
-        id="description"
-        cols="30"
-        rows="10"
-        defaultValue={defaultData?.description}
-      ></textarea>
-      <br />
-
-      {/* <button type="submit">
+        {/* <button type="submit">
         {defaultData ? "Update service" : "Add service"}
       </button> */}
-      <button type="submit">Submit</button>
+        <button type="submit">Submit</button>
+      </Grid>
     </form>
   );
 }
