7ee6dde8abc2dff8b8c1ef11fba5bdad982a3cbc
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-28 20:10:48 +0200

    remove unused code

--- pages/_app.js
+++ pages/_app.js
@@ -3,31 +3,6 @@ import { SWRConfig } from "swr";
 import Main_Layout from "../components/layouts/Main_Layout";
 import { SessionProvider } from "next-auth/react";
 
-// export default function App({
-//   Component,
-//   pageProps: { session, ...pageProps },
-// }) {
-//   return (
-//     <SessionProvider session={session}>
-//       <SWRConfig
-//         value={{
-//           fetcher: async (...args) => {
-//             const response = await fetch(...args);
-//             if (!response.ok) {
-//               throw new Error(`Request with ${JSON.stringify(args)} failed.`);
-//             }
-//             return await response.json();
-//           },
-//         }}
-//       >
-//         <Main_Layout>
-//           <Component {...pageProps} />
-//         </Main_Layout>
-//       </SWRConfig>
-//     </SessionProvider>
-//   );
-// }
-
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
