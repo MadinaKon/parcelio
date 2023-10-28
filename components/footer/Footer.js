ea40adc57604fc3c4dcce969e93035b6bad9f60a
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-28 15:19:26 +0200

    add emoji and dev name to the footer

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,5 +1,9 @@
 import { footer } from "./Footer.module.css";
 
 export default function Footer() {
-  return <footer className={footer}>Made by Spiced 🌶️ Academy graduate</footer>;
+  return (
+    <footer className={footer}>
+      Made by Madina Arstanbekovna Jampasee 👩🏻‍💻❤️{" "}
+    </footer>
+  );
 }
