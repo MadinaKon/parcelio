3efdcca14e61cfdf9e5fbe322d6ec853884f1cb5
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-29 11:27:23 +0100

    use /api/users/ to get badge count user?.notifications?.length

--- components/dashboard/Dashboard.js
+++ components/dashboard/Dashboard.js
@@ -12,6 +12,8 @@ import Link from "@mui/material/Link";
 import MenuIcon from "@mui/icons-material/Menu";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import { useRouter } from "next/router";
+import useSWR from "swr";
+import { useSession } from "next-auth/react";
 
 const drawerWidth = 240;
 
@@ -37,6 +39,11 @@ const AppBar = styled(MuiAppBar, {
 export default function Dashboard({ children }) {
   const [open, setOpen] = useState(false);
   const router = useRouter();
+
+  const { data: session } = useSession();
+  const userId = session?.user?.userId;
+
+  const { data: user } = useSWR(`/api/users/${userId}`);
 
   const handleNotificationClick = () => {
     setOpen(true);
@@ -86,8 +93,7 @@ export default function Dashboard({ children }) {
             {children}
           </Typography>
           <IconButton color="inherit">
-            {/* <Badge badgeContent={99} color="secondary"> */}
-            <Badge badgeContent={99} color="secondary" variant="dot">
+            <Badge badgeContent={user?.notifications?.length} color="secondary">
               <NotificationsIcon onClick={handleNotificationClick} />
             </Badge>
           </IconButton>
