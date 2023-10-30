ead3802038b4bb8dbd185fd88be184e2ddbda285
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-30 12:06:56 +0100

    move ProfileInfo to Nav component

--- components/nav/Nav.js
+++ components/nav/Nav.js
@@ -3,6 +3,7 @@ import { nav } from "./Nav.module.css";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import Dashboard from "../dashboard/Dashboard";
+import { Avatar } from "@mui/material";
 
 export default function Nav() {
   const { data: session } = useSession();
@@ -24,7 +25,10 @@ export default function Nav() {
           {/* TODO here we need authentication, after that http://localhost:3001/services/undefined/updateService will be replaced userId services */}
           {/* <Link href={`/services/${id}/updateService`}>Update service</Link> */}
 
-          <Link href="/profile">Profile</Link>
+          <Link href="/profile">
+            Profile
+            <Avatar alt="Remy Sharp" src={session.user?.image} />
+          </Link>
           {/* <Link href={`/profile/${id}`}>Profile</Link> */}
         </>
       )}
