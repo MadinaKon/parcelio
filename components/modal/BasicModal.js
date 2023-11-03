0dd97b2390dfca3924e4fe9fabc1d8cb31b5c4dc
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-11-03 15:36:51 +0100

    move DeleteModal code inside BasicModal

--- components/modal/BasicModal.js
+++ components/modal/BasicModal.js
@@ -1,4 +1,3 @@
-import * as React from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
@@ -6,7 +5,13 @@ import Modal from "@mui/material/Modal";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { Tooltip } from "@mui/material";
-import DeleteModal from "./DeleteModal";
+import React, { useState } from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import useSWR from "swr";
 
 const style = {
   position: "absolute",
@@ -22,10 +27,11 @@ const style = {
 
 export default function BasicModal({ children, id }) {
   const router = useRouter();
+  const { data: service, mutate } = useSWR(`/api/services`);
 
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  // const handleClose = () => setOpen(false);
 
   const { data: session } = useSession();
 
@@ -35,12 +41,51 @@ export default function BasicModal({ children, id }) {
     }
   };
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  async function deleteService(id) {
+    await fetch(`/api/services/${id}`, {
+      method: "DELETE",
+    });
+
+    mutate();
+    handleClose();
+  }
+
   return (
     <div>
       {session ? (
         <>
           <Button onClick={handleOpen}>Update</Button>
-          <DeleteModal id={id} />
+          <Button variant="contained" color="error" onClick={handleClickOpen}>
+            Delete
+          </Button>
+          <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>Confirm Delete</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                Are you sure you want to delete this item?
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose} color="primary">
+                Cancel
+              </Button>
+              <Button
+                onClick={() => deleteService(id)}
+                color="primary"
+                autoFocus
+              >
+                Confirm
+              </Button>
+            </DialogActions>
+          </Dialog>
         </>
       ) : (
         <a onClick={handleButtonClick}>
