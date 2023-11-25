bf86b3ef1dc44a61a1634e75728064e18d5b8c11
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-11-25 21:50:09 +0100

    change BasicModal top: 50%

--- components/modal/BasicModal.js
+++ components/modal/BasicModal.js
@@ -10,7 +10,7 @@ import DeleteModal from "./DeleteModal";
 
 const style = {
   position: "absolute",
-  top: "30%",
+  top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
   width: 400,
