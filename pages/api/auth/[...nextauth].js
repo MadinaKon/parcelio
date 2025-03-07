a8cf921d9c776dd262e88c7874de204be1210f7b
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2025-03-07 01:39:05 +0100

    add github button

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,23 +8,23 @@ import clientPromise from "../../../db/mongodb.js";
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
-    // GithubProvider({
-    //   clientId: process.env.GITHUB_ID,
-    //   clientSecret: process.env.GITHUB_SECRET,
-    //   // profile: (profile) => {
-    //   //   return {
-    //   //     id: profile.id,
-    //   //     // This ID is required but it will not be saved in your users collection
-    //   //     name: profile.name,
-    //   //     email: profile.email,
-    //   //     image: profile.avatar_url,
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+      // profile: (profile) => {
+      //   return {
+      //     id: profile.id,
+      //     // This ID is required but it will not be saved in your users collection
+      //     name: profile.name,
+      //     email: profile.email,
+      //     image: profile.avatar_url,
 
-    //   //     // You can add any other properties you want to the user object
-    //   //     admin: false,
-    //   //     preferedColors: ["#dddddd", "#ffffff"],
-    //   //   };
-    //   // },
-    // }),
+      //     // You can add any other properties you want to the user object
+      //     admin: false,
+      //     preferedColors: ["#dddddd", "#ffffff"],
+      //   };
+      // },
+    }),
 
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
