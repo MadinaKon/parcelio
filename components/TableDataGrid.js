c78abd091ac59c46a1a2a87ae1ff5d62768ad639
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2025-03-07 02:11:35 +0100

    differ between commonCoumns and userColumns and use useMemo

--- components/TableDataGrid.js
+++ components/TableDataGrid.js
@@ -1,16 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import styled from "styled-components";
-import Link from "next/link.js";
-import { StyledLink } from "../components/StyledLink.js";
+import Link from "next/link";
+import { StyledLink } from "../components/StyledLink";
 import { DataGrid } from "@mui/x-data-grid";
-import SenderForm from "./form/SenderForm.js";
+import SenderForm from "./form/SenderForm";
 import { useRouter } from "next/router";
-import BasicModal from "./modal/BasicModal.js";
+import BasicModal from "./modal/BasicModal";
 import { useSession } from "next-auth/react";
-import TransporterForm from "./form/TransporterForm.js";
+import TransporterForm from "./form/TransporterForm";
 import { Button } from "@mui/material";
 import Tooltip from "@mui/material/Tooltip";
-import Dashboard from "./dashboard/Dashboard.js";
 
 const FixedLink = styled(StyledLink)`
   position: fixed;
@@ -18,65 +17,60 @@ const FixedLink = styled(StyledLink)`
   right: 50px;
 `;
 
+const renderActionsCell = (params, session) => {
+  return (
+    <BasicModal id={params.row._id}>
+      {session ? (
+        <TransporterForm
+          formName={"update-service"}
+          defaultData={params.row}
+          id={params.row._id}
+        />
+      ) : (
+        <SenderForm
+          formName={"add-sender-service"}
+          defaultData={params.row}
+          serviceId={params.row._id}
+          transporterId={params.row.userId[0]}
+        />
+      )}
+    </BasicModal>
+  );
+};
+
+const getCommonColumns = () => [
+  { field: "fromCity", headerName: "From City", width: 150 },
+  { field: "toCity", headerName: "To City", width: 150 },
+  { field: "flightDateTime", headerName: "Flight Date", width: 200 },
+  { field: "availableKilos", headerName: "Available Kilos", width: 150 },
+  {
+    field: "actions",
+    headerName: "Actions",
+    width: 230,
+    renderCell: (params) => renderActionsCell(params, session),
+  },
+];
+
+const getUserColumns = () => [
+  { field: "firstName", headerName: "First Name", width: 150 },
+  { field: "lastName", headerName: "Last Name", width: 150 },
+  { field: "userName", headerName: "Username", width: 150 },
+  { field: "phoneNumber", headerName: "Phone Number", width: 150 },
+];
+
 export default function DataGridComponent({ data }) {
   const router = useRouter();
   const { data: session } = useSession();
   const [columns, setColumns] = useState([]);
 
   useEffect(() => {
+    const commonColumns = getCommonColumns();
     if (session) {
-      setColumns([
-        { field: "firstName", headerName: "First Name", width: 150 },
-        { field: "lastName", headerName: "Last Name", width: 150 },
-        { field: "userName", headerName: "Username", width: 150 },
-        { field: "phoneNumber", headerName: "Phone Number", width: 150 },
-        { field: "fromCity", headerName: "From City", width: 150 },
-        { field: "toCity", headerName: "To City", width: 150 },
-        { field: "flightDateTime", headerName: "Flight Date", width: 200 },
-        { field: "availableKilos", headerName: "Available Kilos", width: 150 },
-        {
-          field: "actions",
-          headerName: "Actions",
-          width: 230,
-          renderCell: (params) => renderActionsCell(params, session),
-        },
-      ]);
+      setColumns([...getUserColumns(), ...commonColumns]);
     } else {
-      setColumns([
-        { field: "fromCity", headerName: "From City", width: 150 },
-        { field: "toCity", headerName: "To City", width: 150 },
-        { field: "flightDateTime", headerName: "Flight Date", width: 200 },
-        { field: "availableKilos", headerName: "Available Kilos", width: 150 },
-        {
-          field: "actions",
-          headerName: "Actions",
-          width: 230,
-          renderCell: (params) => renderActionsCell(params, session),
-        },
-      ]);
+      setColumns(commonColumns);
     }
   }, [session]);
-
-  const renderActionsCell = (params, session) => {
-    return (
-      <BasicModal id={params.row._id}>
-        {session ? (
-          <TransporterForm
-            formName={"update-service"}
-            defaultData={params.row}
-            id={params.row._id}
-          />
-        ) : (
-          <SenderForm
-            formName={"add-sender-service"}
-            defaultData={params.row}
-            serviceId={params.row._id}
-            transporterId={params.row.userId[0]}
-          />
-        )}
-      </BasicModal>
-    );
-  };
 
   const handleButtonClick = (event) => {
     if (!session) {
