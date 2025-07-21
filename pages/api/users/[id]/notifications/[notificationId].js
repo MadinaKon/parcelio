63b5fd6627ad7766a0b56b7d13381f29ae7f5c22
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2025-07-21 16:34:46 +0200

    populate notifications before sending to the client

--- pages/api/users/[id]/notifications/[notificationId].js
+++ pages/api/users/[id]/notifications/[notificationId].js
@@ -13,13 +13,21 @@ export default async function handler(request, response) {
         return response.status(404).json({ error: "User not found" });
       }
 
+      // const updatedUser = await User.findByIdAndUpdate(
+      //   id,
+      //   {
+      //     $pull: { notifications: notificationId }, // Remove string from array
+      //   },
+      //   { new: true }
+      // );
+
       const updatedUser = await User.findByIdAndUpdate(
         id,
         {
           $pull: { notifications: notificationId }, // Remove string from array
         },
         { new: true }
-      );
+      ).populate("notifications");
 
       response.status(200).json({
         status: `notification with id ${notificationId} successfully deleted for userId ${id}`,
