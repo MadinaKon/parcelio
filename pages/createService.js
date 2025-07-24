82ae7d040f5d60d279959bf917fd76cacb658070
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2025-07-24 13:54:59 +0200

    removed unused imports

--- pages/createService.js
+++ pages/createService.js
@@ -1,11 +1,10 @@
 import Link from "next/link.js";
 import styled from "styled-components";
 import { StyledLink } from "../components/StyledLink.js";
-import useSWR from "swr";
 import TransporterForm from "../components/form/TransporterForm.js";
 import SenderForm from "../components/form/SenderForm.js";
 import { useSession } from "next-auth/react";
-import { useState } from "react";
+
 const StyledBackLink = styled(StyledLink)`
   justify-self: flex-start;
 `;
