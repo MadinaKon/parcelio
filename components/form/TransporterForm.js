09c0a7893943ad10e19ccb76677c76f7ed67f87c
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2025-07-24 12:51:15 +0200

    removed useless assignment to variable service

--- components/form/TransporterForm.js
+++ components/form/TransporterForm.js
@@ -39,7 +39,7 @@ const StyledTextArea = styled.textarea`
 
 export default function TransporterForm({ formName, defaultData, id }) {
   const router = useRouter();
-  const { data: service, mutate } = useSWR("/api/services");
+  const { mutate } = useSWR("/api/services");
   const { data: session } = useSession();
 
   const [open, setOpen] = useState(true);
