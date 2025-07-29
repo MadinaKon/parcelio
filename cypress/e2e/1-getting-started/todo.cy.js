40ba667b12dc00300f5b6eda75043792cf358166
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2025-07-29 17:24:12 +0200

    added first cypress regarding include signin

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -6,63 +6,62 @@ describe("parcelio", () => {
   });
 
   it("displays two todo items by default", () => {
-    cy.get(".todo-list li").should("have.length", 2);
-    cy.get(".todo-list li").first().should("have.text", "Pay electric bill");
-    cy.get(".todo-list li").last().should("have.text", "Walk the dog");
+    cy.get("[data-cy='nav-sign-in']").click();
+    cy.url().should("include", "/signin");
   });
 
-  it("can add new todo items", () => {
-    const newItem = "Feed the cat";
-    cy.get("[data-test=new-todo]").type(`${newItem}{enter}`);
-    cy.get(".todo-list li")
-      .should("have.length", 3)
-      .last()
-      .should("have.text", newItem);
-  });
+  // it("can add new todo items", () => {
+  //   const newItem = "Feed the cat";
+  //   cy.get("[data-test=new-todo]").type(`${newItem}{enter}`);
+  //   cy.get(".todo-list li")
+  //     .should("have.length", 3)
+  //     .last()
+  //     .should("have.text", newItem);
+  // });
 
-  it("can check off an item as completed", () => {
-    cy.contains("Pay electric bill")
-      .parent()
-      .find("input[type=checkbox]")
-      .check();
+  // it("can check off an item as completed", () => {
+  //   cy.contains("Pay electric bill")
+  //     .parent()
+  //     .find("input[type=checkbox]")
+  //     .check();
 
-    cy.contains("Pay electric bill")
-      .parents("li")
-      .should("have.class", "completed");
-  });
+  //   cy.contains("Pay electric bill")
+  //     .parents("li")
+  //     .should("have.class", "completed");
+  // });
 
-  context("with a checked task", () => {
-    beforeEach(() => {
-      cy.contains("Pay electric bill")
-        .parent()
-        .find("input[type=checkbox]")
-        .check();
-    });
+  // context("with a checked task", () => {
+  //   beforeEach(() => {
+  //     cy.contains("Pay electric bill")
+  //       .parent()
+  //       .find("input[type=checkbox]")
+  //       .check();
+  //   });
 
-    it("can filter for uncompleted tasks", () => {
-      cy.contains("Active").click();
-      cy.get(".todo-list li")
-        .should("have.length", 1)
-        .first()
-        .should("have.text", "Walk the dog");
-      cy.contains("Pay electric bill").should("not.exist");
-    });
+  //   it("can filter for uncompleted tasks", () => {
+  //     cy.contains("Active").click();
+  //     cy.get(".todo-list li")
+  //       .should("have.length", 1)
+  //       .first()
+  //       .should("have.text", "Walk the dog");
+  //     cy.contains("Pay electric bill").should("not.exist");
+  //   });
 
-    it("can filter for completed tasks", () => {
-      cy.contains("Completed").click();
-      cy.get(".todo-list li")
-        .should("have.length", 1)
-        .first()
-        .should("have.text", "Pay electric bill");
-      cy.contains("Walk the dog").should("not.exist");
-    });
+  //   it("can filter for completed tasks", () => {
+  //     cy.contains("Completed").click();
+  //     cy.get(".todo-list li")
+  //       .should("have.length", 1)
+  //       .first()
+  //       .should("have.text", "Pay electric bill");
+  //     cy.contains("Walk the dog").should("not.exist");
+  //   });
 
-    it("can delete all completed tasks", () => {
-      cy.contains("Clear completed").click();
-      cy.get(".todo-list li")
-        .should("have.length", 1)
-        .should("not.have.text", "Pay electric bill");
-      cy.contains("Clear completed").should("not.exist");
-    });
-  });
+  //   it("can delete all completed tasks", () => {
+  //     cy.contains("Clear completed").click();
+  //     cy.get(".todo-list li")
+  //       .should("have.length", 1)
+  //       .should("not.have.text", "Pay electric bill");
+  //     cy.contains("Clear completed").should("not.exist");
+  //   });
+  // });
 });
