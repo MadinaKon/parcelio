3b226ff4335cdfbdcbdd80001626c10f25d72e6c
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2025-08-01 17:26:48 +0200

    added email login w/o verification

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -37,10 +37,14 @@
 // }
 
 // Example: set session before visiting the app
-Cypress.Commands.add('loginBySession', () => {
-  cy.request('POST', 'https://github.com/u2f/login_fragment?is_emu_login=false&mobile_ios=false&return_to=%2Flogin%2Foauth%2Fauthorize%3Fclient_id%3Db96f8d84172b01b6462c%26redirect_uri%3Dhttp%253A%252F%252Flocalhost%253A3000%252Fapi%252Fauth%252Fcallback%252Fgithub%26response_type%3Dcode%26scope%3Dread%253Auser%2Buser%253Aemail%26state%3DoBw7uo-6zlHPg2VUrNLgrHoXTh4ybrVumSIc4yBwtPI', {
-    user: 'konurbaevamadina+github@gmail.com'
-  }).then((response) => {
-    window.localStorage.setItem('auth_token', response.body.token)
-  })
-})
+Cypress.Commands.add("loginBySession", () => {
+  cy.request(
+    "POST",
+    "https://github.com/u2f/login_fragment?is_emu_login=false&mobile_ios=false&return_to=%2Flogin%2Foauth%2Fauthorize%3Fclient_id%3Db96f8d84172b01b6462c%26redirect_uri%3Dhttp%253A%252F%252Flocalhost%253A3000%252Fapi%252Fauth%252Fcallback%252Fgithub%26response_type%3Dcode%26scope%3Dread%253Auser%2Buser%253Aemail%26state%3DoBw7uo-6zlHPg2VUrNLgrHoXTh4ybrVumSIc4yBwtPI",
+    {
+      user: "konurbaevamadina+github@gmail.com",
+    }
+  ).then((response) => {
+    window.localStorage.setItem("auth_token", response.body.token);
+  });
+});
