33238c91b0b06b06d6050c1bc7ff32b130244eaf
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2025-08-03 19:28:59 +0200

    installed cypress-mailslurp

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -35,29 +35,3 @@
 //     }
 //   }
 // }
-
-// Example: set session before visiting the app
-// Cypress.Commands.add("loginBySession", () => {
-//   cy.request(
-//     "POST",
-//     "https://github.com/u2f/login_fragment?is_emu_login=false&mobile_ios=false&return_to=%2Flogin%2Foauth%2Fauthorize%3Fclient_id%3Db96f8d84172b01b6462c%26redirect_uri%3Dhttp%253A%252F%252Flocalhost%253A3000%252Fapi%252Fauth%252Fcallback%252Fgithub%26response_type%3Dcode%26scope%3Dread%253Auser%2Buser%253Aemail%26state%3DoBw7uo-6zlHPg2VUrNLgrHoXTh4ybrVumSIc4yBwtPI",
-//     {
-//       user: "konurbaevamadina+github@gmail.com",
-//     }
-//   ).then((response) => {
-//     window.localStorage.setItem("auth_token", response.body.token);
-//   });
-// });
-
-Cypress.Commands.add(
-  "loginBySession",
-  (email = Cypress.env("TEST_USER_EMAIL")) => {
-    cy.request("POST", Cypress.env("GITHUB_LOGIN_URL"), {
-      user: email,
-    }).then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body).to.have.property("token");
-      window.localStorage.setItem("auth_token", response.body.token);
-    });
-  }
-);
--- cypress/support/index.d.ts
+++
@@ -1,9 +0,0 @@
-// cypress/support/index.d.ts
-
-/// <reference types="cypress" />
-
-declare namespace Cypress {
-  interface Chainable<Subject = any> {
-    loginBySession(): Chainable<void>;
-  }
-}
